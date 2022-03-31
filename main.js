b5d702dbce6c534c6267e134554e930016f94071
1 <a1@1s-MacBook-Pro.local>
2022-03-31 11:57:21 -0400

    rev1

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 // Setup 'tick' sound
-const tick = new Audio('sounds/tick.mp3');
+const tick = new Audio("sounds/tick.mp3");
+const tock = new Audio("sounds/tock.mp3");
+let count = 0;
 
 // This function is called every 600ms
 function update() {
-
-    // Play the 'tick' sound
+  // Play the 'tick' sound
+  count++;
+  if (count % 4 === 0) {
+    tock.play();
+  } else {
     tick.play();
+  }
 }
 
 // This function sets up update() to be called every 600ms
 function setupUpdate() {
-    setInterval(update, 600);
+  setInterval(update, 600);
 }
 
 // Call setupUpdate() once after 300ms
